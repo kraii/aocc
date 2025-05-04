c5167b391c92c7145182f42b31887ebb4beca66f
kraii <matthewkerry@gmail.com>
2025-05-04 14:53:55 +0100

    More strings

--- strings.c
+++ strings.c
@@ -4,46 +4,114 @@
 #include <stdlib.h>
 #include <bits/signum-generic.h>
 
-enum String_status {
-    Success = 0,
-    Bounds_Check_Failure
-};
-
 struct string {
     size_t cap;
     size_t len;
-    char buffer[];
+    char buffer[/*cap*/];
 };
 
 typedef struct string string;
 
-string *new_empty_string(const size_t capacity) {
-    // Allocate 1 extra char for \0
-    string *result = malloc(offsetof(string, buffer) + (sizeof(char) * (capacity + 1)));
-    result->cap = capacity;
-    result->len = 0;
-    result->buffer[0] = '\0';
-    return result;
+static void terminate(string* s) {
+    s->buffer[s->len] = '\0';
 }
 
-int string_set(string *s, const char *value, const size_t len) {
-    if (len > s->cap) {
-        raise(SIGTRAP);
-        return Bounds_Check_Failure;
-    }
 
-    memcpy(s->buffer, value, len * sizeof(char));
-    s->buffer[len] = '\0';
-    s->len = len;
-    return Success;
-}
-
-string *new_string(const size_t capacity, const char*value, const size_t len) {
-    string *s = new_empty_string(capacity);
-    if (string_set(s, value, len) != Success) {
-        free(s);
+/**
+ * Allocates a new string with the given capacity (>0) and length 0
+ */
+string *string_new_empty(const size_t capacity) {
+    if (capacity == 0) {
         return NULL;
     }
 
+    // Allocate 1 extra char for \0
+    string *result = malloc(offsetof(string, buffer) + (sizeof(char) * (capacity + 1)));
+    if (result == NULL) {
+        raise(SIGTRAP);
+        return NULL;
+    }
+
+    result->cap = capacity;
+    result->len = 0;
+    terminate(result);
+    return result;
+}
+
+
+static size_t min(const size_t a, const size_t b) {
+    return b < a ? b : a;
+}
+
+/**
+ * @return the length of the given string
+ */
+size_t string_len(const string *s) {
+    return s->len;
+}
+
+/**
+ * Sets the contents of the given string to the value of buffer
+ * If len > capacity will only copy up to capacity
+ */
+void string_set(string *s, const char *value, const size_t len) {
+    memcpy(s->buffer, value, min(len, s->cap) * sizeof(char));
+    s->len = min(len, s->cap);
+    terminate(s);
+}
+
+/**
+ * Allocates a new string with the value of the given buffer
+ * If len > capacity will only copy up to capacity
+ */
+string *string_new(const size_t capacity, const char*value, const size_t len) {
+    string *s = string_new_empty(capacity);
+    string_set(s, value, len);
     return s;
+}
+
+/**
+ * Allocates a new string that is a copy of the given string
+ * If len of the original > capacity will only copy up to capacity
+ */
+string *string_copy(const size_t capacity, const string *original) {
+    return string_new(capacity, original->buffer, min(capacity, original->len));
+}
+
+#define string_set_l(s, value) string_set(s, value, sizeof(value)-1)
+#define string_new_l(value) string_new(sizeof(value) - 1, value, sizeof(value) -1)
+
+/**
+ * @return A newly allocated string with the result of concatenating l and r
+ */
+string *string_cat_new(const string *l, const string *r) {
+    string *result = string_copy(l->len + r->len, l);
+    memcpy(result->buffer + l->len, r->buffer, r->len * sizeof(char));
+    result->len = l->len + r->len;
+    terminate(result);
+    return result;
+}
+
+/**
+ * @return The remaining capacity of the given string
+ */
+size_t string_rem_cap(const string *s) {
+    return s->cap - s->len;
+}
+
+/**
+ * Write the result of concatenating l and r into l
+ * will truncate at the max capacity of l
+ */
+void string_cat_l(string *l, const string *r) {
+    const size_t rem_cap = string_rem_cap(l);
+    if (rem_cap <= 0) {
+        // No space to concat anything so give up
+        return;
+    }
+    const size_t to_cat = min(rem_cap, r->len);
+    memcpy(l->buffer + l->len, r->buffer, to_cat * sizeof(char));
+    l->len = l->len + to_cat;
+
+    terminate(l);
 }
--- strings_test.c
+++ strings_test.c
@@ -5,7 +5,7 @@
 #include "strings.c"
 
 Test(strings, new_empty_string) {
-    string* s = new_empty_string(5);
+    string* s = string_new_empty(5);
 
     cr_assert(eq(s->cap, 5));
     cr_assert(eq(s->len, 0));
@@ -15,9 +15,38 @@ Test(strings, new_empty_string) {
 }
 
 Test(strings, set_string) {
-    string* s = new_empty_string(5);
-    const int status = string_set(s, "12345", 5);
-    cr_assert(eq(status, 0));
+    string* s = string_new_empty(5);
+    string_set(s, "12345", 5);
+    cr_assert(eq(s->len, 5));
+    cr_assert_str_eq(s->buffer, "12345");
+    cr_assert(eq(strlen(s->buffer), 5));
+
+    free(s);
+}
+
+Test(strings, set_string_oob) {
+    string* s = string_new_empty(5);
+    string_set(s, "123456", 6);
+    cr_assert(eq(s->len, 5));
+    cr_assert_str_eq(s->buffer, "12345");
+    cr_assert(eq(strlen(s->buffer), 5));
+
+    free(s);
+}
+
+Test(strings, new_string_from_literal) {
+    string *s = string_new(5, "1234", 4);
+
+    cr_assert(eq(s->len, 4));
+    cr_assert_str_eq(s->buffer, "1234");
+    cr_assert(eq(strlen(s->buffer), 4));
+
+    free(s);
+}
+
+Test(strings, set_string_l) {
+    string* s = string_new_empty(5);
+    string_set_l(s, "12345");
     cr_assert(eq(s->len, 5));
 
     cr_assert_str_eq(s->buffer, "12345");
@@ -26,21 +55,65 @@ Test(strings, set_string) {
     free(s);
 }
 
-Test(strings, set_string_oob, .signal = SIGTRAP) {
-    string* s = new_empty_string(5);
-    const int status = string_set(s, "123456", 6);
-    cr_assert(eq(status, 1));
-    cr_assert(eq(s->len, 0));
 
-    free(s);
-}
-
-Test(strings, new_string_from_literal) {
-    string *s = new_string(5, "1234", 4);
+Test(strings, new_string_l) {
+    string *s = string_new_l("1234");
 
     cr_assert(eq(s->len, 4));
     cr_assert_str_eq(s->buffer, "1234");
     cr_assert(eq(strlen(s->buffer), 4));
 
     free(s);
+}
+
+Test(strings, copy) {
+    string *a = string_new_l("1234");
+    string *b = string_copy(3, a);
+
+    cr_assert(eq(b->len, 3));
+    cr_assert_str_eq(b->buffer, "123");
+    cr_assert(eq(strlen(b->buffer), 3));
+
+    free(a);
+    // ReSharper disable once CppDFADeletedPointer
+    free(b);
+}
+
+
+Test(strings, concat_new) {
+    string *a = string_new_l("abc ");
+    string *b = string_new_l("123");
+
+    string *result = string_cat_new(a, b);
+    cr_assert_str_eq(result->buffer, "abc 123");
+    cr_assert(eq(strlen(result->buffer), 7));
+
+    free(a);
+    free(b);
+    free(result);
+}
+
+Test(strings, concat_l) {
+    string *a = string_new(10, "abc ", 4);
+    string *b = string_new_l("123");
+
+    string_cat_l(a, b);
+    cr_assert_str_eq(a->buffer, "abc 123");
+    cr_assert(eq(strlen(a->buffer), 7));
+    cr_assert(eq(a->len, 7));
+    free(a);
+    free(b);
+}
+
+
+Test(strings, concat_l_over_cap) {
+    string *a = string_new(6, "abc ", 4);
+    string *b = string_new_l("123");
+
+    string_cat_l(a, b);
+    cr_assert_str_eq(a->buffer, "abc 12");
+    cr_assert(eq(strlen(a->buffer), 6));
+    cr_assert(eq(a->len, 6));
+    free(a);
+    free(b);
 }
