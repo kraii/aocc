ccc350f2c0c66609a79013c3d8f53ded85278aec
kraii <matthewkerry@gmail.com>
2025-05-20 20:58:17 +0100

    Day 4 pt 2

--- day4_2024.c
+++ day4_2024.c
@@ -51,19 +51,40 @@ int find_xmas(const grid *grid, const int x, const int y) {
   return count;
 }
 
+bool is_x_mas(const grid *grid, const int x, const int y) {
+  const bool possible =
+      x > 0 && x < grid_w(grid) - 1 && y > 0 && y < grid_h(grid) - 1;
+  if (!possible) {
+    return false;
+  }
+  const char ul = grid_at(grid, x - 1, y - 1);
+  const char ur = grid_at(grid, x + 1, y - 1);
+  const char dl = grid_at(grid, x - 1, y + 1);
+  const char dr = grid_at(grid, x + 1, y + 1);
+
+  return ((ul == 'S' && dr == 'M') || (ul == 'M' && dr == 'S')) &&
+         ((ur == 'S' && dl == 'M') || (ur == 'M' && dl == 'S'));
+}
+
 int main(const int argc, char *argv[]) {
   assert(argc > 1);
   const grid *grid = grid_from_file(str_wrap_c(argv[1]));
   unsigned xmas = 0;
+  unsigned x_mas = 0;
   for (int y = 0; y < grid_h(grid); y++) {
     for (int x = 0; x < grid_w(grid); x++) {
       const char tile = grid_at(grid, x, y);
       if (tile == 'X') {
         xmas += find_xmas(grid, x, y);
       }
+      if (tile == 'A' && is_x_mas(grid, x, y)) {
+        x_mas++;
+      }
     }
   }
 
   printf("Part 1 %u \n", xmas);
+  printf("Part 2 %u \n", x_mas);
   assert(xmas == 2524);
+  assert(x_mas == 1873);
 }
