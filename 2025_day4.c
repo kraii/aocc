7ca70c5d5e4ff6c16a618755103902190ccd06f0
kraii <matthewkerry@gmail.com>
2025-12-04 23:23:52 +0000

    Refactor 25-4 (clion did a weird)
    Enable warnings and do the recommended way of setting cstd in meson.

--- 2025_day4.c
+++ 2025_day4.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 #include "grid.h"
-
 #include <stdlib.h>
 
-static void count_adj_paper(const grid *grid, const point current, int *adj_paper) {
-  *adj_paper = 0;
+static int count_adj_paper(const grid *grid, const point current) {
+  int adj_paper = 0;
   for (int dir = 0; dir < 8; dir++) {
     const point adj = point_add(current, grid_directions_8[dir]);
     if (grid_in_boundsp(grid, adj) && grid_atp(grid, adj) == '@') {
-      (*adj_paper)++;
+      adj_paper++;
     }
   }
+  return adj_paper;
 }
 
 static void do_part_1(const grid *grid) {
@@ -25,8 +25,7 @@ static void do_part_1(const grid *grid) {
         continue;
       }
 
-      int adj_paper;
-      count_adj_paper(grid, current, &adj_paper);
+      const int adj_paper =  count_adj_paper(grid, current);
       if (adj_paper < 4) {
         part_1++;
       }
@@ -48,8 +47,7 @@ static void do_part_2(grid *grid) {
           continue;
         }
 
-        int adj_paper;
-        count_adj_paper(grid, current, &adj_paper);
+        const int adj_paper =  count_adj_paper(grid, current);
         if (adj_paper < 4) {
           removed++;
           part_2++;
@@ -64,8 +62,6 @@ static void do_part_2(grid *grid) {
 
   printf("part 2: %d\n", part_2);
 }
-
-
 
 int main(const int argc, char *argv[]) {
   assert(argc > 1);
